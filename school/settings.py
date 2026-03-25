cfb6a0ebf9e5d86db780471e4bdcfeb1c1cdeeec
Nandhiinii <140712774+Nandhiinii@users.noreply.github.com>
2026-03-25 14:01:32 +0530

    Update settings.py

--- school/settings.py
+++ school/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4ofdtln@5b3vrr-mk24=hp9yr5%%y9_iup2p(jf(n!*)$-or&e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['school-sent.onrender.com']
 
 
 # Application definition
