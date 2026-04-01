d44c5c1709d9fb66cffaa6732a291e8fdff6639b
Nandhiinii <140712774+Nandhiinii@users.noreply.github.com>
2026-04-01 21:50:47 +0530

    Update settings.py

--- school/settings.py
+++ school/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4ofdtln@5b3vrr-mk24=hp9yr5%%y9_iup2p(jf(n!*)$-or&e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://school-sent.onrender.com"]
 AUTH_USER_MODEL = 'school_app.CustomUser'
 
 # Application definition
